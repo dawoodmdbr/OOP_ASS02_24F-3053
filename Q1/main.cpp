14df09dbcb7e39ec2824395bc6b07c01a5cca05a
dawoodmdbr <dawoodbinrafaydbr@gmail.com>
2025-03-22 20:38:09 +0500

    Added unit and coordinates struct

--- Q1/main.cpp
+++ Q1/main.cpp
@@ -1,5 +1,39 @@
 #include<iostream>
 using namespace std;
+struct coordinates{
+    int x;
+    int y;
+};
+struct Unit{
+    string unitType;
+    int health;
+    int attack;
+    int defense;
+    int movementRange;
+    struct coordinates position;
+    Unit(string u, int h, int a, int d, int m, int x, int y){
+        unitType = u;
+        health = h;
+        attack = a;
+        defense = d;
+        movementRange = m;
+        position.x = x;
+        position.y = y;
+    }
+    void moveUnit(int newX, int newY){
+        position.x = newX;
+        position.y = newY;
+    }
+    bool isAlive(){
+        return (health > 0);
+    }
+    void attackUnit(Unit* enemyUnit){
+        int damage = attack - enemyUnit->defense;
+        if(damage > 0){
+            enemyUnit->health -= damage;
+        }
+    }
+};
 int main(){
     cout<<"Hello World!"<<endl;
     return 0;
