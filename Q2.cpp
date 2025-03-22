030f6d1578f627e0b44cc4aa54d88de318cd1a48
dawoodmdbr <dawoodbinrafaydbr@gmail.com>
2025-03-23 02:52:14 +0500

    Added main code

--- Q2.cpp
+++ Q2.cpp
@@ -59,13 +59,13 @@ struct Character{
         if (experience >= 100){
             level++;
             experience-=100;
-            cout << "Leveled up to " << level << endl;
+            cout << "Leveled up to " << level << endl << "Experience: " << experience << endl << endl;
         }
     }
     void useItem(Item*item){
         if(item->type == POTION){
             health += item->value;
-            cout << "Health: " << health << endl;
+            cout << "Used " << item->name << endl << "Health: " << health << endl << endl;
         }
     }
     void displayCharacterInfo(){
@@ -79,6 +79,7 @@ struct Character{
         for(int i = 0; i < inventorySize; i++){
             cout << "Item " << i+1 << ": " << inventory[i]->name << endl;
         }
+        cout<< endl;
     }
 };
 
@@ -102,17 +103,80 @@ struct Terrain{
         cout << "Type: " << type << endl;
         cout << "Enemies: " << endl;
         for(int i = 0; i < numEnemies; i++){
-            cout << "Enemy " << i+1 << ": " << enemies[i]->name << endl;
+            cout << enemies[i]->name << endl;
         }
         cout << "Items: " << endl;
         for(int i = 0; i < numItems; i++){
-            cout << "Item " << i+1 << ": " << items[i]->name << endl;
+            cout << items[i]->name << endl;
         }
+        cout << endl;
     }
 };
-
+struct Area{
+    string name;
+    struct Terrain *terrain;
+    struct Area* connectedAreas[10];
+    int numConnectedAreas;
+    void addConnectedArea(Area*area){
+        connectedAreas[numConnectedAreas] = area;
+        numConnectedAreas++;
+    }
+    void displayAreaInfo(){
+        cout << "Name: " << name << endl;
+        cout << "Terrain: " << terrain->name << endl;
+        cout << "Connected Areas: " << endl;
+        for(int i = 0; i < numConnectedAreas; i++){
+            cout << "Connected Area " << i+1 << ": " << connectedAreas[i]->name << endl;
+        }
+        cout << endl;
+    }
+};
+struct GameWorld{
+    struct Area *areas[10];
+    int numAreas;
+    void addArea(Area*area){
+        areas[numAreas] = area;
+        numAreas++;
+    }
+    void displayWorldInfo(){
+        cout << "Areas: " << endl;
+        for(int i = 0; i < numAreas; i++){
+            cout << "Area " << i+1 << ": " << endl;
+            areas[i]->displayAreaInfo();
+            cout<< "Terrain "<<i+1<<": " << endl;
+            areas[i]->terrain->displayTerrainInfo();
+        }
+        cout << endl;
+    }
+};
 int main()
 {
-    cout << "Hello World";
+    
+    Item sword = {"Sword", WEAPON, 100, 3.5};
+    Item potion = {"Health Potion", POTION, 50, 0.5};
+    Enemy goblin = {"Goblin", GOBLIN, 50, 10, 2};
+    Terrain forest = {"Enchanted Forest", FOREST, {}, 0, {}, 0};
+    Area village = {"Old Village", &forest, {}, 0};
+    Area cave = {"Dark Cave", &forest, {}, 0};
+    GameWorld world = {{}, 0};
+
+    forest.addEnemy(&goblin);
+    village.terrain->addItem(&sword);
+    world.addArea(&village);
+    village.addConnectedArea(&cave);
+    cave.addConnectedArea(&village);
+
+    
+    struct Character hero = {"Knight", 1, 0, 100, 15, 5, {}, 0};
+    hero.inventory[hero.inventorySize] = &potion;
+    hero.inventorySize++;
+    hero.inventory[hero.inventorySize] = &sword;
+    hero.inventorySize++;
+    hero.displayCharacterInfo();
+    hero.gainExperience(120);
+    hero.useItem(&potion);
+    
+    world.displayWorldInfo();
+    
     return 0;
 }
